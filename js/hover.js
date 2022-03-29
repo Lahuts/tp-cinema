8929b49ce087639ecf507ed7118470c9dbee3078
Lahuts <albumcroquis@gmail.com>
2022-03-29 23:55:41 +0200

    Update_modal_rsp_nd_rw

--- js/hover.js
+++ js/hover.js
@@ -5,20 +5,23 @@ document.addEventListener("DOMContentLoaded", e => {
         title: "Lucy",
         date: "2014",
         director: "Luc Besson",
-        type: "science-fiction"
+        type: "science-fiction",
+        trailer: "https://www.youtube.com/embed/bN7ksFEVO9U"
     }, {
         image: "https://www.themoviedb.org/t/p/w1280/ndI4tgEtyztuhracdyiTNDYuhEl.jpg",
         title: "Vikings",
         date: "2013",
         director: "Michael Hirst",
-        type: "historique"
+        type: "historique",
+        trailer: "https://www.youtube.com/embed/9GgxinPwAGc"
 
     }, {
         image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSbIzUFFVjbCs3bNx3px0sdT38SoU5hrG3mhV5hrUsieuwz62tc",
         title: "Avengers EndGame",
         date: "2019",
         director: "Anthony et Joe Russo",
-        type: "action , super-héros"
+        type: "action , super-héros",
+        trailer: "https://www.youtube.com/embed/o3Hs5WZd8l4"
     }, {
 
         title: "Joker",
@@ -27,7 +30,8 @@ document.addEventListener("DOMContentLoaded", e => {
         duree: "2h2",
         acteurs: "Joaquin Phoenix",
         director: "Todd Phillips",
-        image: "https://www.themoviedb.org/t/p/w1280/dCp0auHcFum1g6N1YSMLQmyVCT6.jpg"
+        image: "https://www.themoviedb.org/t/p/w1280/dCp0auHcFum1g6N1YSMLQmyVCT6.jpg",
+        trailer: "https://www.youtube.com/embed/zAGVQLHvwOY"
     }, {
 
         title: "Batman",
@@ -36,79 +40,88 @@ document.addEventListener("DOMContentLoaded", e => {
         duree: "2h55",
         acteurs: "Robert Pattinson",
         director: "Matt Reeves",
-        image: "https://www.themoviedb.org/t/p/w1280/krbjW0bbXSdeysYGMrdfpcJy6PH.jpg"
+        image: "https://www.themoviedb.org/t/p/w1280/krbjW0bbXSdeysYGMrdfpcJy6PH.jpg",
+        trailer: "https://www.youtube.com/embed/mqqft2x_Aa4"
     }, {
 
         title: "Le voyage de Chihiro",
-        date: "",
+        date: "2001",
         type: "Fantastique",
-        duree: "2h5min",
-        acteurs: "",
         director: "Hayao Miyazaki",
-        image: "https://images.affiches-et-posters.com//albums/3/7390/medium/AF7303.JPG"
+        image: "https://images.affiches-et-posters.com//albums/3/7390/medium/AF7303.JPG",
+        trailer: "https://www.youtube.com/embed/5-cro68n7CE"
     }, {
 
         title: "Flash",
         date: "2014",
         type: "Science-Fiction",
         director: "Greg Berlanti",
-        image: "https://www.themoviedb.org/t/p/w1280/hF2hIInMmr4XOuMBx4uo3pElzz2.jpg"
+        image: "https://www.themoviedb.org/t/p/w1280/hF2hIInMmr4XOuMBx4uo3pElzz2.jpg",
+        trailer: "https://www.youtube.com/embed/Yj0l7iGKh8g"
     }, {
 
         title: "Jujustu Kaisen 0",
         date: "2022",
         type: "Animation",
         director: "Sunghoo Park",
-        image: "https://www.themoviedb.org/t/p/w1280/4MAIb2ctURfs9YNIcEzk0IS4M5I.jpg"
+        image: "https://www.themoviedb.org/t/p/w1280/4MAIb2ctURfs9YNIcEzk0IS4M5I.jpg",
+        trailer: "https://www.youtube.com/embed/-iun6KPT4SM"
     }, {
 
         title: "Peaky Blinders",
         date: "2013",
         type: "Drame",
         director: "Steven Knight",
-        image: "https://www.themoviedb.org/t/p/w1280/vUUqzWa2LnHIVqkaKVlVGkVcZIW.jpg"
+        image: "https://www.themoviedb.org/t/p/w1280/vUUqzWa2LnHIVqkaKVlVGkVcZIW.jpg",
+        trailer: "https://www.youtube.com/embed/oVzVdvGIC7U"
     }, {
 
         title: "Uncharted",
         date: "2022",
         type: "Aventure",
         director: "Ruben Fleischer",
-        image: "https://www.themoviedb.org/t/p/w1280/sqLowacltbZLoCa4KYye64RvvdQ.jpg"
+        image: "https://www.themoviedb.org/t/p/w1280/sqLowacltbZLoCa4KYye64RvvdQ.jpg",
+        trailer: "https://www.youtube.com/embed/eHp3MbsCbMg"
     }, {
 
         title: "The Walking Dead",
         date: "2010",
         type: "Action & Aventure",
         director: "Frank Darabont",
-        image: "https://www.themoviedb.org/t/p/w1280/yTNP9UOdUbGBUZ7SSVng75IwAaY.jpg"
+        image: "https://www.themoviedb.org/t/p/w1280/yTNP9UOdUbGBUZ7SSVng75IwAaY.jpg",
+        trailer: "https://www.youtube.com/embed/sfAc2U20uyg"
     }, {
 
         title: "Game of Thrones",
         date: "2011",
         type: "Fantastique",
         director: "David Benioff",
-        image: "https://www.themoviedb.org/t/p/w1280/mMHYHIiC9I2ghn12Fnr4ImvWWss.jpg"
+        image: "https://www.themoviedb.org/t/p/w1280/mMHYHIiC9I2ghn12Fnr4ImvWWss.jpg",
+        trailer: "https://www.youtube.com/embed/KYKpcWuZDYs"
     }, {
 
         title: "Euphoria",
         date: "2019",
         type: "Drame",
         director: "Sam Levinson",
-        image: "https://www.themoviedb.org/t/p/w1280/5o6f5tAj0yJaVrx6fdmwNDZiojA.jpg"
+        image: "https://www.themoviedb.org/t/p/w1280/5o6f5tAj0yJaVrx6fdmwNDZiojA.jpg",
+        trailer: "https://www.youtube.com/embed/UR4Pxgnm-GA"
     }, {
 
         title: "Le loup de Wall Street",
         date: "2013",
         type: "Comédie",
         director: "Martin Scorsese",
-        image: "https://www.themoviedb.org/t/p/w1280/dQIQZbJXn1pflQw3nwvXLJX0dHa.jpg"
+        image: "https://www.themoviedb.org/t/p/w1280/dQIQZbJXn1pflQw3nwvXLJX0dHa.jpg",
+        trailer: "https://www.youtube.com/embed/iszwuX1AK6A"
     }, {
 
         title: "The Last Kingdom",
         date: "2015",
         type: "Action",
         director: "Stéphan Butchard",
-        image: "https://www.themoviedb.org/t/p/w1280/wUFIEJCSmurmXPX0mP0cEgoltrk.jpg"
+        image: "https://www.themoviedb.org/t/p/w1280/wUFIEJCSmurmXPX0mP0cEgoltrk.jpg",
+        trailer: "https://www.youtube.com/embed/0ZXdPhfMbAw"
     }];
 
     class Movie {
@@ -118,21 +131,24 @@ document.addEventListener("DOMContentLoaded", e => {
             this.date = mov.date; // li
             this.director = mov.director; // li
             this.type = mov.type; // li
+            this.trailer = mov.trailer; // li
             this.printObj();
         }
         printObj(movies) {
             ul.innerHTML += `
-        <li>
+        <li 
+        data-title="${this.title}"
+        data-date="${this.date}"
+        data-director="${this.director}"
+        data-type="${this.type}"
+        data-image="${this.image}"
+        data-trailer="${this.trailer}"
+        >
            <figure>
-           <img id="test" src="${this.image}"></img>
+           <img class="affiche" src="${this.image}"></img>
              <figcaption>
                 <h3>${this.title}</h3>
              </figcaption>
-             <ul>
-                   <li>${this.director}</li>
-                    <li>${this.date}</li>
-                    <li>${this.type}</li>
-                 </ul>
             </figure>
         </li>
        `;
@@ -146,14 +162,4 @@ document.addEventListener("DOMContentLoaded", e => {
     for (let i = 0; i < movies.length; i++) {
         let film = new Movie(movies[i]);
     }
-    document.querySelectorAll('#test').forEach(item => {
-        item.addEventListener('mouseenter', event => {
-            //handle click
-        });
-    });
-    document.querySelectorAll('#test').forEach(item => {
-        item.addEventListener('mouseout', event => {
-            //handle click
-        });
-    });
 });
---
+++ js/modale.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", e => {
+  // pensez a modifier hover.js pour que ce code fonctionne
+  // Modal windows of the movies on the home page
+
+  console.log(navigator.userAgent);
+});
+
+document.addEventListener("DOMContentLoaded", e => {
+  console.log(navigator.userAgent);
+
+  let el, modal, closed, open_modal, closed_all, footer_right;
+
+  el = document.querySelectorAll("#grid ul li");
+  modal = document.querySelector(".bc-modal");
+  closed = document.querySelector(".close");
+
+  open_modal = function () {
+    console.log(this.dataset);
+
+    let trailer = this.dataset.trailer;
+    let title = this.dataset.title;
+    let director = this.dataset.director;
+    let date = this.dataset.date;
+    let type = this.dataset.type;
+    modal.classList.add("modale-active"); /* ajouter la classe active */
+    /* sélectionner les sélecteurs html*/
+    document.querySelector(".fr-modal iframe").src = trailer;
+    document.querySelector(".modal-info h3").innerText = title;
+    document.querySelector(".modal-info .li1").innerText = "Réalisateur : " + director;
+    document.querySelector(".modal-info .li2 ").innerText = "Genre : " + type;
+    document.querySelector(".modal-info li time").innerText = "Date de sortie : " + date;
+  };
+  for (rows of el) {
+    rows.addEventListener("click", open_modal);
+  }
+  closed.addEventListener("click", () => {
+    modal.classList.remove("modale-active");
+    document.querySelector(".fr-modal iframe").src = "";
+  });
+});
